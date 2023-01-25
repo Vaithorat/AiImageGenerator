58d99060c41a5614273a86a9b56e6bb07d37493d
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-01-25 11:19:33 +0530

    redeployed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   const fetchPosts = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`https://ai-image-generator-qv02.onrender.com/api/v1/post`, {
+      const response = await fetch(`https://dalleimage.onrender.com/api/v1/post`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
