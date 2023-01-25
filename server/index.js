db3cf3d103b497157f2fdcf589de754bc0e3945d
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-01-25 12:00:03 +0530

    bugs

--- server/index.js
+++ server/index.js
@@ -14,17 +14,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
-  });
+app.get("/", async (req, res) => {
+  res.status(200).json("Hello from DALL.E!");
 });
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () =>
-      console.log("Server has started on port 8080")
-    );
+    app.listen(8080, () => console.log("Server has started on port 8080"));
   } catch (error) {
     console.log(error);
   }
