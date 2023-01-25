a8e570b14d953dbc9da27778f80f7008bfe3f79f
Vaithorat <vaibhavthorat141997@gmail.com>
2023-01-25 10:26:25 +0530

    fixes

--- server/index.js
+++ server/index.js
@@ -14,10 +14,11 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", async (req, res) => {
-  res.send("Hello from DALL-E");
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
 });
-
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
